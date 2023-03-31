0162ba37b6cb6109b465c348ffd7f09eee923f05
Abinithishkumarprabakaran <abinithishkumarp.a.k@gmail.com>
2023-03-31 15:30:08 +0530

    Pop Up Functionality

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import {Popup} from './components/Popup'
+import { useState, useEffect } from 'react';
+
+function App() {
+
+  const [buttonPopup, setButtonPopup] = useState(false)
+  const [timedPopup, setTimedPopup] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimedPopup(true);
+    }, 3000);
+  }, []);
+
+  return (
+    <div className="App">
+      <main>
+        <h1>React Popups</h1>
+        <br></br>
+        <button onClick={() => setButtonPopup(true)}>Open Popup</button>
+      </main>
+
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+          <h3>My Popup</h3>
+          <p>This is my button trigger popup</p>
+      </Popup>
+
+      <Popup trigger={timedPopup} setTrigger={setTimedPopup}>
+          <h3>My Popup</h3>
+          <p>This is my time triggered popup</p>
+      </Popup>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Popup.jsx
@@ -0,0 +1,16 @@
+export function Popup(props) {
+
+  return (props.trigger) ? (
+    <div className="popup">
+      <div className="popup-inner">
+        <button 
+          className="close-btn" 
+          onClick={() => props.setTrigger(false)}
+          >
+          close
+        </button>
+        {props.children}
+      </div>
+    </div>
+  ) : "";
+}
